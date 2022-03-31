e649741bee76391733f6fe4a47d30761b0a2ff02
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-03-31 11:25:54 +0200

    init

---
+++ path-finder.py
@@ -0,0 +1,27 @@
+import curses
+from curses import wrapper
+import queue
+import time
+
+maze = [
+    ["#", "O", "#", "#", "#", "#", "#", "#", "#"],
+    ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
+    ["#", " ", "#", "#", " ", "#", "#", " ", "#"],
+    ["#", " ", "#", " ", " ", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", "#", "#"],
+    ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
+    ["#", "#", "#", "#", "#", "#", "#", "X", "#"]
+]
+
+def main(stdscr):
+    curses.init_pair(1, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
+    # magenta_and_black = curses.color_pair(1)
+    # stdscr.clear()
+    # stdscr.addstr(5,5, "hello world!", magenta_and_black)
+    # stdscr.refresh()
+    # stdscr.getch()
+
+wrapper(main)
