53e8cca967ec690823e8d5b046e9b54c60cb5301
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-04-05 10:30:06 +0200

    added wrapper and print_maze

--- path-finder.py
+++ path-finder.py
@@ -15,13 +15,29 @@ maze = [
     ["#", "#", "#", "#", "#", "#", "#", "X", "#"]
 ]
 
+def print_maze(maze, stdscr, path=[]):
+    BLUE = curses.color_pair(1)
+    RED = curses.color_pair(2)
+
+    for i, row in enumerate(maze):
+        for j, value in enumerate(row):
+            stdscr.addstr(i,j, value)
+    
+
+
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
-    # magenta_and_black = curses.color_pair(1)
-    # stdscr.clear()
-    # stdscr.addstr(5,5, "hello world!", magenta_and_black)
-    # stdscr.refresh()
-    # stdscr.getch()
+            # magenta_and_black = curses.color_pair(1)
+            # stdscr.clear()
+            # stdscr.addstr(5,5, "hello world!", magenta_and_black)  # row, column, print scr., color
+            # stdscr.refresh()
+            # stdscr.getch()  # get character - wait for user to hit anything to exit the program
+    stdscr.clear()
+    print_maze(maze, stdscr)
+    stdscr.refresh()
+    stdscr.getch()
+    
 
 wrapper(main)
